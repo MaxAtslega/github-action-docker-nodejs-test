d634684d62158bc2921c53bda44d7aeaf0a8f7f9
Max Atslega <47830961+MaxAtslega@users.noreply.github.com>
2021-02-19 21:34:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res, next) {
-    res.send('Hello Jupiter!');
+    res.send('Hello Mars!');
 });
 
 
